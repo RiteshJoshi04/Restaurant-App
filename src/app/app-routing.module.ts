fcea99318cbccb34d72bc3e48b5130a74cdb8a22
Ritesh Joshi <riteshjoshi022@gmail.com>
2020-11-18 10:27:45 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateRestaurantComponent } from './update-restaurant/update-restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: AddRestaurantComponent, path: 'add'},
+  {component: UpdateRestaurantComponent, path: 'update'},
+  {component: ListeningStateChangedEvent, path: 'list'},
+  {component: LoginComponent, path: 'login'},
+  {component: RegisterComponent, path: 'register'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
